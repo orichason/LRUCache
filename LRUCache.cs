2bf49c5daac7cd3f087f9e97f8875501d7049d3b
Ori Chason <orichason@gmail.com>
2024-03-01 16:01:09 -0800

    Add project files.

---
+++ LRUCache.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LRUCache
+{
+    public class LRUCache<T>
+    {
+        public interface ICache<TKey, TValue>
+        {
+            bool TryGetValue(TKey key, out TValue value);
+            void Put(TKey key, TValue value);
+        }
+
+        internal enum Operations
+        {
+            Add,
+            Subtract,
+            Multiply,
+            Divide
+        };
+        internal struct cacheMaterial
+        {
+            int a;
+            int b;
+            Operations operation;
+        }
+
+        Dictionary<cacheMaterial, T> hashMap;
+
+        LinkedList<T> LRU;
+
+        public LRUCache()
+        {
+            hashMap = new();
+            LRU = new();
+        }
+    }
+}
