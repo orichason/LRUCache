80ae91d8d051798944fe7dfa5eafdd0aab01aa54
Ori Chason <orichason@gmail.com>
2024-03-08 16:05:35 -0800

    Worked on ICache. Almost done with LRU just need to fix minor errors

--- LRUCache.cs
+++ LRUCache.cs
@@ -7,36 +7,102 @@ using System.Threading.Tasks;
 
 namespace LRUCache
 {
-    public class LRUCache<T>
+    internal enum Operations
     {
-        public interface ICache<TKey, TValue>
-        {
-            bool TryGetValue(TKey key, out TValue value);
-            void Put(TKey key, TValue value);
-        }
+        Add,
+        Subtract,
+        Multiply,
+        Divide
+    };
+    internal struct Equation
+    {
+        int a;
+        int b;
+        Operations operation;
+    }
+    public interface ICache<TKey, TValue>
+    {
+        bool TryGetValue(TKey key, out TValue value);
+        void Put(TKey key, TValue value);
+    }
 
-        internal enum Operations
-        {
-            Add,
-            Subtract,
-            Multiply,
-            Divide
-        };
-        internal struct cacheMaterial
-        {
-            int a;
-            int b;
-            Operations operation;
-        }
+    public class LRUCache<TKey, TValue> : ICache<TKey, TValue>
+    {
+        //struct Container
+        //{
+        //    public TKey Key;
+        //    public TValue Value;
+        //    public Container(TKey key, TValue value)
+        //    {
+        //        Key = key;
+        //        Value = value;
+        //    }
+        //}
 
-        Dictionary<cacheMaterial, T> hashMap;
+        readonly Dictionary<TKey, LinkedListNode<KeyValuePair<TKey, TValue>>> hashMap;
 
-        LinkedList<T> LRU;
+        readonly LinkedList<KeyValuePair<TKey, TValue>> LRU;
 
-        public LRUCache()
+        int capacity;
+        public LRUCache(int capacity)
         {
-            hashMap = new();
+            this.capacity = capacity;
+            hashMap = new(capacity);
             LRU = new();
+        }
+
+        void MoveNodeToFront(LinkedListNode<KeyValuePair<TKey, TValue>> node)
+        {
+            LRU.Remove(node);
+            LRU.AddFirst(node);
+        }
+
+        /// <summary>
+        /// If key exists, move it to front of list and update value.
+        /// If key doesn't exist, create new node and add to front of list.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        void ICache<TKey, TValue>.Put(TKey key, TValue value)
+        {
+            if (hashMap.ContainsKey(key))
+            {
+                hashMap[key].Value.Value = value;
+                MoveNodeToFront(hashMap[key]);
+            }
+
+            else
+            {
+                if (hashMap.Count == capacity)
+                {
+                    var nodeToRemove = LRU.Last;
+
+                }
+                var newNode = new LinkedListNode<TValue>(value);
+                hashMap.Add(key, newNode);
+                LRU.AddFirst(newNode);
+            }
+        }
+
+        /// <summary>
+        /// Given a key, if it exists inside the hash map (cache hit), move the node to the head of the list. 
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="value"></param>
+        /// <returns> value of node and whether key exists</returns>
+        bool ICache<TKey, TValue>.TryGetValue(TKey key, out TValue value)
+        {
+            if (hashMap.ContainsKey(key))
+            {
+                var nodeToMove = hashMap[key];
+                MoveNodeToFront(nodeToMove);
+
+                value = nodeToMove.Value;
+                return true;
+            }
+
+            value = default!;
+            return false;
         }
     }
 }
